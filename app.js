08aeb6288792a240afd4db042ad0fd75743e7b26
Rajdeep Ghosh <rajdeepghosh810@gmail.com>
2021-02-17 21:49:06 +0530

    Setup ejs

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import ejs from "ejs";
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
 
